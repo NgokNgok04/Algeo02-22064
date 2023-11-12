6a6966abdb8f4460580542fe7cc804ff79f67c3e
Devinzenzhang <devinzenzhang@gmail.com>
2023-11-12 23:55:11 +0700

    fix: optimization using numpy

--- src/CBIR_warna.py
+++ src/CBIR_warna.py
@@ -1,30 +1,20 @@
 from PIL import Image
-def open_image(image_location):
+import numpy as np
+def open_image_and_preprocess(image_location):
     img = Image.open(image_location) # path
-    width, height = img.size[0], img.size[1]
-    NewW, NewH = width // 4, height // 4
-    f = len(img.getpixel((0, 0))) # 3 - jpg, 4 - png
-    arr = [[[0 for _ in range (3)] for _ in range (NewH)] for _ in range (NewW)] # apakah ada cara lebih bagus
-    # sudah sekaligus dibagi jadi blok 4x4
-    # kalau tidak habis dibagi 4 terpotong
-    if (f == 3):
-        for i in range (width):
-            for j in range (height):
-                for k in range (3):
-                    arr[i // 4][j // 4][k] += img.getpixel((i, j))[k]
-    else:
-        for i in range (width):
-            for j in range (height):
-                for k in range (3):
-                    arr[i // 4][j // 4][k] += (img.getpixel((i, j))[k]) * ((img.getpixel((i, j))[3]) / 255)
-    for i in range(NewW):
-        for j in range (NewH):
-            for k in range (3):
-                arr[i][j][k] = arr[i][j][k] / 16
-            
-            # convert to hsv sekalian
+    arr = np.array(img)
+
+    # membagi menjadi blok 4x4 dan rata-rata rgb, untuk gambar png komponen a diabaikan
+    NHeight, NWidth = (img.size[1] - (img.size[1] % 4)), (img.size[0] - (img.size[0] % 4))
+    arr = arr[:NHeight, :NWidth, :3] # cut jika tidak habis dibagi 4 biar tidak pusing mikirin, cut a jika png
+    arr = arr.reshape((NHeight // 4), 4, (NWidth // 4), 4, 3) # compress 4x4
+    arr = arr.mean(axis = (1, 3))
+
+    # ubah dari rgb ke hsv (fungsi rgb_to_hsv dibikin sendiri (salin rumus dari spesifikasi) RIL NO FEK NO FEK)
+    for i in range (len(arr)):
+        for j in range (len(arr[0])):
             arr[i][j][0], arr[i][j][1], arr[i][j][2] = rgb_to_hsv(arr[i][j][0], arr[i][j][1], arr[i][j][2])
-    return arr # arr[width][height] = [h, s, v]
+    return arr # arr[height][width] = [h, s, v], float
 
 def rgb_to_hsv(r, g, b):
     R, G, B = r / 255, g / 255, b / 255
