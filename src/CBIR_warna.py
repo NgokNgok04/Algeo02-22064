19a7aa971f12659461ba3ef15e56d464c7cf91ec
Devinzenzhang <devinzenzhang@gmail.com>
2023-11-13 11:08:55 +0700

    feat: hsv color histogram

--- src/CBIR_warna.py
+++ src/CBIR_warna.py
@@ -7,15 +7,12 @@ def open_image_and_preprocess(image_location):
     # compress
     arr = compress(arr)
 
-    # ubah dari rgb ke hsv (fungsi rgb_to_hsv dibikin sendiri (salin rumus dari spesifikasi) RIL NO FEK NO FEK)
-    Height, Width = len(arr), len(arr[0])
-    r, g, b = arr[..., 0], arr[..., 1], arr[..., 2]
-    r, g, b = r.reshape(Height * Width), g.reshape(Height * Width), b.reshape(Height * Width)
-    hsv = [rgb_to_hsv(R, G, B) for R, G, B in zip(r, g, b)]
-    h, s, v = map(np.array, zip(*hsv))
-    h, s, v = h.reshape(Height, Width), s.reshape(Height, Width), v.reshape(Height, Width)
-    arr = np.stack([h, s, v], axis=-1)
+    # ubah dari rgb ke histogram hsv
+    arr = rgb_to_hsv_histBinArray(arr)
+    bins = [i for i in range (65)]
+    arr, _ = np.histogram(arr, bins = bins)
 
+    # arr array 1D dengan panjang 64, tinggal dipakai untuk cosine similarity
     return arr
 
 def compress(Image_Array):
@@ -28,6 +25,7 @@ def compress(Image_Array):
     return Compressed_Image_Array
 
 def rgb_to_hsv(r, g, b):
+    # ubah dari rgb ke hsv (fungsi rgb_to_hsv dibikin sendiri (salin rumus dari spesifikasi) RIL NO FEK NO FEK)
     R, G, B = r / 255, g / 255, b / 255
     Cmax, Cmin = max(R, G, B), min(R, G, B)
     d = Cmax - Cmin
@@ -49,5 +47,14 @@ def hsv_to_histidx(h, s, v):
     idx = (H * 8) + (S * 4) + V # idx 0 - 63
     return idx
 
-arr = open_image_and_preprocess("../test/Jadwal_Clan_Training_3.png")
-# python3 CBIR_warna.py
+def rgb_to_hsv_histBinArray(rgb_array):
+    Height, Width = len(rgb_array), len(rgb_array[0])
+    r, g, b = rgb_array[..., 0], rgb_array[..., 1], rgb_array[..., 2] # r, g, b itu array 2D yang isinya r, g, b dari gambar
+    r, g, b = r.reshape(Height * Width), g.reshape(Height * Width), b.reshape(Height * Width) # jadi array 1D
+
+    # mengubah jadi array of tuple HSV
+    hsv = np.array([rgb_to_hsv(R, G, B) for R, G, B in zip(r, g, b)], dtype = [('x', float), ('y', float), ('z', float)])
+
+    hist_idx = np.array([hsv_to_histidx(h, s, v) for h, s, v in hsv])
+
+    return hist_idx
