ba528503a1354754edeb7a5fa9850743d3a67b9d
unknown <117185466+NgokNgok04@users.noreply.github.com>
2023-11-18 23:46:40 +0700

    feat: Add upload dataset w Flask

---
+++ src/flask-server/app.py
@@ -0,0 +1,70 @@
+from flask import Flask, json, request, jsonify
+import os
+# import urlib.request import urlopen
+from werkzeug.utils import secure_filename
+from flask_cors import CORS 
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['txt,''pdf','png','jpg','jpeg','gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'files[]' not in request.files:
+        resp = jsonify({
+            "message": 'No file part in the request',
+            "status": 'failed'
+        })
+        resp.status_code = 400
+        return resp
+    
+    files = request.files.getlist('files[]')
+
+    errors = {}
+    success = False
+
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            success = True
+        else:
+            resp = jsonify({
+                "message": 'Files successfully uploaded',
+                "status": 'success'
+            })
+            return resp
+    
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        errors['status'] = 'failed'
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    if success:
+        resp = jsonify({
+            "message": 'Files successfully uploaded',
+            "status": 'successs'
+        })
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
--- src/website-algeo/src/App.js
+++ src/website-algeo/src/App.js
@@ -1,21 +1,24 @@
 // App.js
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom';
-import Home from './Home';
 import About from './AboutUs';
 import HowToUse from './HowtoUse';
 import Program from './Program';
+import ImageUpload from './components/ImageUpload';
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Program />}/>
-        <Route path="/How-to-Use" element = {<HowToUse />}/>
-        <Route path="/about" element={<About />}/>
-      </Routes>
-    </Router>
-  );
-};
+class App extends Component {
+  render(){
+      return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Program />}/>
+            <Route path="/How-to-Use" element = {<HowToUse />}/>
+            <Route path="/about" element={<About />}/>
+            <Route exact path = "/upload-image" element={<ImageUpload />} />
+          </Routes>
+        </Router>
+      );
+  }
+}
 
 export default App;
